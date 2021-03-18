31a29ef3257e848d586d2f64093687c5a0064ee7
Zach Kodjo <36774583+kodjoz@users.noreply.github.com>
2021-03-18 19:31:07 -0400

    Revised sql query to just import data

--- database/import_data.sql
+++ database/import_data.sql
@@ -1,15 +1,15 @@
 USE sdcProduct;
 -- Import product data
-LOAD DATA LOCAL INFILE '/home/kodjoz/hackreactor/sdc-products/data/product.csv' REPLACE INTO TABLE sdcProduct.Products FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' IGNORE 1 LINES (productId, name, slogan, description, category, defaultPrice);
--- -- Import Features
-LOAD DATA LOCAL INFILE '/home/kodjoz/hackreactor/sdc-products/data/features.csv' REPLACE INTO TABLE sdcProduct.Features FIELDS TERMINATED BY ',' ENCLOSED BY '"' (featureId, productId, feature, value);
--- Import styles
-LOAD DATA LOCAL INFILE '/home/kodjoz/hackreactor/sdc-products/data/styles.csv' REPLACE INTO TABLE sdcProduct.Styles FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' IGNORE 1 LINES (styleId, productId, name, salePrice, originalPrice, defaultStyle);
--- Import photos
-LOAD DATA LOCAL INFILE '/home/kodjoz/hackreactor/sdc-products/data/photos-005.csv' REPLACE INTO TABLE sdcProduct.Photos FIELDS TERMINATED BY ',' IGNORE 1 LINES (photoId, styleId, url, thumbnailUrl);
--- Import related
-LOAD DATA LOCAL INFILE '/home/kodjoz/hackreactor/sdc-products/data/related.csv' REPLACE INTO TABLE sdcProduct.Related FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' IGNORE 1 LINES (relatedid, currentProductId, relatedProductId);
+-- LOAD DATA LOCAL INFILE '/home/kodjoz/hackreactor/sdc-products/data/product.csv' REPLACE INTO TABLE sdcProduct.Products FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' IGNORE 1 LINES (productId, name, slogan, description, category, defaultPrice);
+-- -- -- Import Features
+-- LOAD DATA LOCAL INFILE '/home/kodjoz/hackreactor/sdc-products/data/features.csv' REPLACE INTO TABLE sdcProduct.Features FIELDS TERMINATED BY ',' ENCLOSED BY '"' (featureId, productId, feature, value);
+-- -- Import styles
+-- LOAD DATA LOCAL INFILE '/home/kodjoz/hackreactor/sdc-products/data/styles.csv' REPLACE INTO TABLE sdcProduct.Styles FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' IGNORE 1 LINES (styleId, productId, name, salePrice, originalPrice, defaultStyle);
+-- -- Import photos
+-- LOAD DATA LOCAL INFILE '/home/kodjoz/hackreactor/sdc-products/data/photos-005.csv' REPLACE INTO TABLE sdcProduct.Photos FIELDS TERMINATED BY ',' IGNORE 1 LINES (photoId, styleId, url, thumbnailUrl);
+-- -- Import related
+-- LOAD DATA LOCAL INFILE '/home/kodjoz/hackreactor/sdc-products/data/related.csv' REPLACE INTO TABLE sdcProduct.Relateds FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' IGNORE 1 LINES (relatedid, currentProductId, relatedProductId);
 -- Import SKUs
-LOAD DATA LOCAL INFILE ‘/home/kodjoz/hackreactor/SDC-API/data/skus.csv’ REPLACE INTO TABLE sdcProduct.SKUs FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' IGNORE 1 LINES (skuId, styleId, size, quantity);
+LOAD DATA LOCAL INFILE '/home/kodjoz/hackreactor/SDC-API/data/skus.csv' REPLACE INTO TABLE sdcProduct.SKUs FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' IGNORE 1 LINES (skuId, styleId, size, quantity);
 
 
