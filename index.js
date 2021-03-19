7e20b3cf4cfbc3cf1d2f0d41c9cb3d0034c6a7f3
Zach Kodjo <36774583+kodjoz@users.noreply.github.com>
2021-03-19 13:48:03 -0400

    Added new routes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
-const { getProducts } = require('./database/query');
+const { getProducts, getOneProduct, getRelatedProducts } = require('./database/query');
 
 const app = express();
 const port = 3000;
@@ -25,6 +25,40 @@ app.get('/products', (req, res) => {
     })
     .catch((error) => {
       console.error('There was an error fetching from the database', error);
-      res.status(500).send('Error');
+      res.status(500).send(`Error getting information on products from page ${page} and count ${count}`);
+    });
+});
+
+app.get('/products/:product_id/', (req, res) => {
+  const product = Number(req.params.product_id);
+
+  return getOneProduct(product)
+    .then((results) => {
+      console.log(results);
+      res.status(200).send(results);
+    })
+    .catch((error) => {
+      console.error('There was an error fetching from the database', error);
+      res.status(500).send(`Error getting info on product ${product}`);
+    });
+});
+
+// related
+app.get('/products/:product_id/related', (req, res) => {
+  return getRelatedProducts(Number(req.params.product_id))
+    .then((results) => {
+      let resultArray = [];
+      
+      // transform it into a bare array
+      for (var i = 0; i < results.length; i++) {
+        let current = results[i];
+        resultArray.push(results[i]['related_product_id']);
+      }
+
+      res.status(200).send(resultArray);
+    })
+    .catch((error) => {
+      console.error('There was an error fetching from the database', error);
+      res.status(500).send('Error getting related products');
     });
 });
