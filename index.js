e24d6d26ff73f52e01168e76533c51900b2bfb82
Zach Kodjo <36774583+kodjoz@users.noreply.github.com>
2021-03-18 20:50:33 -0400

    Added query function to first API route

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const { getProducts } = require('./database/query');
 
 const app = express();
 const port = 3000;
@@ -12,4 +13,18 @@ app.listen(port, () => {
 // default route
 app.get('/', (req, res) => {
   res.send('Hello from SDC Products endpoint');
+});
+
+app.get('/products', (req, res) => {
+  const page = Number(req.query.page);
+  const count = Number(req.query.count);
+
+  return getProducts(page, count)
+    .then((results) => {
+      res.status(200).send(results);
+    })
+    .catch((error) => {
+      console.error('There was an error fetching from the database', error);
+      res.status(500).send('Error');
+    });
 });
