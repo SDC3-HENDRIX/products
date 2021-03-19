634edc68bfb594c2362cbabcbc73193434ade653
Zach Kodjo <36774583+kodjoz@users.noreply.github.com>
2021-03-19 13:47:39 -0400

    Created getOneProduct and getRelatedProducts

--- database/query.js
+++ database/query.js
@@ -1,4 +1,4 @@
-const { Product } = require('./model.js');
+const { Product, Feature, Related } = require('./model.js');
 
 // create a query
 // get products by page and count
@@ -11,15 +11,30 @@ exports.getProducts = (page, count) => {
     pageOffset = 0;
   }
   return Product.findAll({
-    attributes: ['product_id', 'name', 'slogan', 'description', 'category', 'default_price'],
+    attributes: { exclude: ['createdAt', 'updatedAt'], },
     offset: pageOffset,
     limit: count,
   })
     .catch((error) => console.error('Error when querying products', error));
 };
 
-exports.getOneProduct = (productId) => Product.findAll({
-  attributes: ['product_id', 'name', 'slogan', 'description', 'category', 'default_price'],
-  include: [{ model: 'Feature', where: 'product_id' }],
-})
-  .catch((error) => console.error(`Error when querying ${productId}`, error));
+exports.getOneProduct = (productId) => {
+  return Product.findOne({
+    attributes: { exclude: ['createdAt', 'updatedAt'] },
+    where: {
+      product_id: productId,
+    },
+    include: {
+      model: Feature,
+      attributes: ['feature', 'value'],
+    },
+  });
+}
+
+exports.getRelatedProducts = (productId) => Related.findAll({
+  attributes: ['related_product_id'],
+  where: {
+    current_product_id: productId,
+  },
+  order: ['related_product_id'],
+});
