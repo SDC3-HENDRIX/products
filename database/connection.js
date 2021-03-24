d136b502e189623e351295bd2fe1c2cca6b3f5c1
Zach Kodjo <36774583+kodjoz@users.noreply.github.com>
2021-03-24 13:44:34 -0400

    Converts connection to docker and adds max pools ize

--- database/connection.js
+++ database/connection.js
@@ -8,9 +8,13 @@ const dbUser = process.env.DBUSER;
 const dbPass = process.env.NODE_ENV === 'production' ? process.env.DBPASS_PROD : process.env.DBPASS_DEV;
 
 const db = new Sequelize(dbName, dbUser, dbPass, {
-  host: 'localhost',
+  host: 'host.docker.internal',
   dialect: 'mariadb',
   logging: (msg) => logger.debug(msg),
+  pool: {
+    max: 80,
+    min: 0,
+  },
 });
 
 module.exports = db;
